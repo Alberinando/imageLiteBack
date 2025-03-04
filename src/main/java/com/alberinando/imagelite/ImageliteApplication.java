5fe015dc39748400c3b5052c77ca9a3c7c85052a
Albernando <alberinandocipriano@hotmail.com>
2025-03-04 17:15:45 -0300

    first commit

---
+++ src/main/java/com/alberinando/imagelite/ImageliteApplication.java
@@ -0,0 +1,13 @@
+package com.alberinando.imagelite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageliteApplication.class, args);
+	}
+
+}
